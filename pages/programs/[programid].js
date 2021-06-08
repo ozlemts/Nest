8c84b70d8d4a9441f297c1f785ef8bf0ac5f32e7
ozlemts <ozlemturkerii@gmail.com>
2021-06-08 11:30:06 +0300

    env variables and sample file is added

--- pages/programs/[programid].js
+++ pages/programs/[programid].js
@@ -7,7 +7,10 @@ function ProgramPage() {
   const router = useRouter();
   console.log(router.pathname);
   console.log(router.query);
-  const { data, error } = useSWR(`/api/programs/{$router.query}`, fetcher)
+
+  const fetcher = (url) => fetch(url).then(res => res.json())
+
+  const { data, error } = useSWR(`https://jsonplaceholder.typicode.com/todos/1`, fetcher)
 
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
@@ -17,7 +20,7 @@ function ProgramPage() {
       <div className="text-center mx-auto max-w-2xl">
         <h1>{router.query.programname}</h1>m
         <div className="">
-          <p className="text-2xl font-bold mt-8">hello {data.name}!</p>
+          <p className="text-2xl font-bold mt-8">hello {data.title}!</p>
           <p className="mt-4">
             Duis eu velit tempus erat egestas efficitur. In hac habitasse platea
             dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra
