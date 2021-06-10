cc6505ce799c637ef6c2081cdeffeba02ceddae6
ozlemts <ozlemturkerii@gmail.com>
2021-06-10 11:00:03 +0300

    airtable api is added for programs table

--- pages/programs/[programid].js
+++ pages/programs/[programid].js
@@ -1,26 +1,16 @@
-import useSWR from 'swr'
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 
-
 function ProgramPage() {
   const router = useRouter();
-  console.log(router.pathname);
-  console.log(router.query);
-
-  const fetcher = (url) => fetch(url).then(res => res.json())
-
-  const { data, error } = useSWR(`https://jsonplaceholder.typicode.com/todos/1`, fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
 
   return (
     <Layout>
       <div className="text-center mx-auto max-w-2xl">
-        <h1>{router.query.programname}</h1>m
+        <p>{router.query.programid}</p>
         <div className="">
-          <p className="text-2xl font-bold mt-8">hello {data.title}!</p>
+          <p className="text-2xl font-bold mt-8">
+          </p>
           <p className="mt-4">
             Duis eu velit tempus erat egestas efficitur. In hac habitasse platea
             dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra
--- pages/programs/index.js
+++ pages/programs/index.js
@@ -1,45 +1,9 @@
 import Layout from "@/components/Layout";
-import Link from "next/link";
 import ProgramCard from "@/components/ProgramCard";
 import Button from "@/components/Button";
+import { getTable } from "/pages/api/programs";
 
-const programs = [
-  {
-    name: "Coco Cola",
-    type: "nest-in",
-    url: "",
-    logo: "/moon.svg",
-    darklogo: "/moon.svg",
-    status: "Tamamlandı",
-    duration: "2021-2022",
-    properties:
-      "Konsept geliştirme,Landing page ve kullanıcı arayüzü,MvP/Prototip/Demo üretme ve müşteri lead toplama",
-  },
-  {
-    name: "Coco Cola",
-    type: "nest-in",
-    url: "https://viveka.com.tr/",
-    logo: "cola.svg (https://dl.airtable.com/.attachments/1c1f35c0edc86a14bef36f6f931ac420/b6b9d32a/cola.svg)",
-    darklogo:
-      "cola.svg (https://dl.airtable.com/.attachments/9238cb142b0e258d620a530d2573a279/7787d948/cola.svg)",
-    status: "Tamamlandı",
-    duration: "2010-2011",
-    properties:
-      "Konsept geliştirme,Landing page ve kullanıcı arayüzü,MvP/Prototip/Demo üretme ve müşteri lead toplama",
-    aim: "Duis eu velit tempus erat egestas efficitur. In hac habitasse platea dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra quis lacus at viverra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \n\nMorbi efficitur auctor metus, id mollis lorem pellentesque id. Nullam posuere maximus dui et fringilla.",
-    scope:
-      "Duis eu velit tempus erat egestas efficitur. In hac habitasse platea dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra quis lacus at viverra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \n\nMorbi efficitur auctor metus, id mollis lorem pellentesque id. Nullam posuere maximus dui et fringilla.",
-    partners: "odtu,izmiryuksekteknoloji",
-    output: "Yenilikçi Bakış Açısı,Sistemetik Yaklaşım",
-    tags: "product,MVP",
-  },
-];
-
-programs.forEach((e) => {
-  e.properties = e.properties.split(",");
-  e.logo = e.logo.replace(/.*\(|\).*/g, "");
-});
-function ProgramsPage() {
+function ProgramsPage({ data }) {
   return (
     <Layout>
       <div className="text-center">
@@ -59,18 +23,26 @@ function ProgramsPage() {
           <Button type="btn-tab">Ön Kuluçka</Button>
         </div>
       </div>
-      <div className="flex flex-wrap justify-center">
-        <ProgramCard program={programs[0]} />
-        <ProgramCard program={programs[1]} />
-        <ProgramCard program={programs[0]} />
-        <ProgramCard program={programs[0]} />
-        <ProgramCard program={programs[0]} />
-        <ProgramCard program={programs[0]} />
-        <ProgramCard program={programs[0]} />
-        <ProgramCard program={programs[0]} />
-      </div>
+      {data.length > 0 && (
+        <div className="flex flex-wrap justify-center">
+          {data.map((p, index) => (
+            <ProgramCard key={index} program={p.fields} />
+          ))}
+        </div>
+      )}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const data = await getTable("programs");
+
+  return {
+    props: {
+      data: data,
+    },
+    revalidate: 600,
+  };
 }
 
 export default ProgramsPage;
