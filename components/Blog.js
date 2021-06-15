5b4640a9ee34b7757944a76216e5e908d2c0a598
ozlemts <ozlemturkerii@gmail.com>
2021-06-15 12:00:44 +0300

    blog sample data is added

--- components/Blog.js
+++ components/Blog.js
@@ -2,6 +2,46 @@ import Button from "@/components/Button";
 import Link from "next/link";
 import BlogCard from "@/components/BlogCard";
 
+const BlogData = [
+  {
+    title: "Covid-19 Sonrası Hızla Değişen Girişimcilik Trendleri",
+    url: "https://blog.viveka.com.tr/covid-19-sonrasi-hizla-degisen-girisimcilik-trendleri/",
+    description:
+      "Covid-19 sonrası Girişimcilik alanında en çok ne gibi aksiyonlar alındığını, bu konuda önümüzdeki yıl bizi hangi trendlerin beklediğini ekibimizden Beril Karabulut yazdı.",
+    date: "18 Aralık 2020",
+    img_url:
+      "https://blog.viveka.com.tr/content/images/size/w2000/2020/12/pexels-photo-56759.jpeg",
+  },
+  {
+    title: "Girişim Gelişim Döngüleri",
+    url: "https://blog.viveka.com.tr/girisim-gelisim-donguleri/",
+    description:
+      "Sevgili Zoom`un yorgun savaşçıları...Uzaktan çalışmaya geçtiğimiz zamandan beri bir seneden fazla geçti. Geçirdiğimiz dönemin zorlukları ve ortak kayıplarımız gölgesinde yazı yazmak eskisinden biraz daha güç. ",
+    date: "8 Haziran 2021",
+    img_url:
+      "https://blog.viveka.com.tr/content/images/size/w2000/2021/06/pexels-daniel-torobekov-4886378-1.jpg",
+  },
+  {
+    title: "10 Saniye Yeni Ömür Uzunluğumuz Mu?",
+    url: "https://blog.viveka.com.tr/yeni-omur-uzunlugu/",
+    description:
+      "Her geçen gün dikkatlerimiz daha da azalıyor, siz de farkında mısınız? Belki bu yazıyı okurken, belki bir film izlerken, belki sevdiklerimizle sohbet ederken..  2000’li yılların başlarında yetişkin bir insanın bir konuyla alakalı olarak dikkat süresi yaklaşık 12 saniye iken, son yıllarda bu süre 8 saniyeye indi.",
+    date: "6 Haziran 2021",
+    img_url:
+      "https://blog.viveka.com.tr/content/images/size/w2000/2021/06/maxresdefault.jpeg",
+  },
+  {
+    title:
+      "Kadın Girişimcinin Gözünden: Bize yine meşakkatli toplumsal cinsiyet yolları",
+    url: "https://blog.viveka.com.tr/kadingirisimciningozunden/",
+    description:
+      "‘UN Women’ın yürüttüğü araştırmalar gösterdi ki 2020 boyunca ve hala devam eden Covid-19’tan kadınlar erkeklerden daha olumsuz yönde etkileniyor’. Bu haberi okuyunca gülümseyip ‘Hangi konuda daha az etkileniyor ki zaten’ demiştim içimden.",
+    date: "7 Mayıs 2021",
+    img_url:
+      "https://blog.viveka.com.tr/content/images/size/w2000/2021/05/diverse-women-illustration.jpeg",
+  },
+];
+
 function Blog() {
   return (
     <section
@@ -13,26 +53,25 @@ function Blog() {
         <div className="shadow-lg flex flex-wrap rounded dark:bg-blue-500">
           <div className="lg:w-3/5 p-8 text-left">
             <p className="text-2xl font-bold dark:text-blue-100">
-              Long established
+              {BlogData[0].title}
             </p>
-            <p className="mt-8 text-gray-400">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that....
-            </p>
+            <p className="mt-8 text-gray-400">{BlogData[0].description}</p>
             <div className="flex justify-between mt-3">
-              <p>20 Mayıs 2021</p>
+              <p>{BlogData[0].date}</p>
               <div className="font-bold">
-                <Link href="https://blog.viveka.com.tr/">Daha Fazla</Link>
+                <Link href={BlogData[0].url}>Daha Fazla</Link>
               </div>
             </div>
           </div>
-          <img src="/sample.png" className="lg:w-2/5" />
+          <img
+            src={BlogData[0].img_url}
+            className="lg:w-2/5 h-72 object-cover"
+          />
         </div>
         <div className="flex flex-wrap justify-between my-8">
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
+          <BlogCard blogdata={BlogData[1]} />
+          <BlogCard blogdata={BlogData[2]} />
+          <BlogCard blogdata={BlogData[3]} />
         </div>
         <div className="my-6 text-center">
           <Button type="btn-primary" shadow>
--- components/BlogCard.js
+++ components/BlogCard.js
@@ -3,20 +3,16 @@ import Link from "next/link";
 function BlogCard(props) {
   return (
     <div className="rounded-lg shadow-lg hover:shadow w-96 text-left dark:bg-blue-500">
-      <img src="/sample.png" className="w-full" />
-      <div className="p-5 ">
+      <img src={props.blogdata.img_url} className="w-full h-56 object-cover" />
+      <div className="p-5 flex flex-col justify-between h-96">
         <p className="text-xl font-bold my-4 dark:text-blue-100">
-          It is a long established{" "}
+          {props.blogdata.title}
         </p>
-        <p className="text-gray-400">
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that....
-        </p>
+        <p className="text-gray-400">{props.blogdata.description}</p>
         <div className="flex justify-between mt-3">
-          <p>20 Mayıs 2021</p>
+          <p>{props.blogdata.date}</p>
           <div className="font-bold">
-            <Link href="https://blog.viveka.com.tr/">
+            <Link href={props.blogdata.url}>
               <p className="hover:text-red-500">Daha Fazla</p>
             </Link>
           </div>
