89271483b8def61d8d8e222c894f098ee81c3012
ozlemts <ozlemturkerii@gmail.com>
2021-06-15 15:32:58 +0300

    footer is updated for new design

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,18 @@
 import Logo from "@/components/Logo";
+import Facebook from "@/components/Facebook";
 
 function Footer() {
   return (
     <div className="w-full mt-12 bg-blue-50 dark:bg-blue-700 text-center">
-
-      <div className="px-3 lg:px-36 md:px-12 flex justify-between p-6 ">
+      <div className="px-3 lg:px-36 md:px-12 flex justify-between p-6 text-gray-500">
         <Logo />
-        <div>vff</div>
+        <div>
+          <Facebook />
+        </div>
       </div>
-      <p className="text-sm text-gray-500 bg-white p-1">2021@NEST</p>
+      <p className="text-sm text-gray-500 bg-white dark:bg-blue-500 p-1">
+        2021@NEST
+      </p>
     </div>
   );
 }
