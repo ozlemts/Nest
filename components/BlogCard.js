3350b9f290dbcb4e13f7f22c49a8803561c6ed69
ozlemts <ozlemturkerii@gmail.com>
2021-07-02 11:29:29 +0300

    blog height problem has solved

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,12 +1,12 @@
 function BlogCard(props) {
   return (
-    <div className="rounded-lg shadow-lg hover:shadow text-left dark:bg-blue-500">
+    <div className="rounded-lg shadow-lg hover:shadow text-left dark:bg-blue-500 flex flex-col justify-between">
       <img src={props.blogdata.img_url} className="w-full h-56 object-cover" />
-      <div className="p-5 flex flex-col justify-between h-96">
+      <div className="p-5 flex flex-col justify-between">
         <p className="text-xl font-bold my-4 dark:text-blue-100">
           {props.blogdata.title}
         </p>
-        <p className="text-gray-400 flex-grow">{props.blogdata.description}</p>
+        <p className="text-gray-400">{props.blogdata.description}</p>
         <div className="flex justify-between mt-3">
           <p>{props.blogdata.date}</p>
           <div className="font-bold">
